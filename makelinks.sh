460755282a8026546f1a9d3744a79b71c41b4a09
Ted Schundler <tschundler@truecar.com>
2015-12-15 17:04:17 -0800

    Sublime Text on OSX

---
+++ makelinks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+BASEDIR=$(pwd)
+
+link_recurse() {
+    local DEST SRC n DEST_NAME
+    DEST=${*: -1:1}
+    n=${#*}
+    # shell array voodoo... shouldn't it be n-1?
+    for SRC in ${*::n}; do
+    	DEST_NAME=$(basename "$SRC")
+        if [ -d "$SRC" ]; then
+            #echo $SRC is DIR
+            mkdir -p "$DEST/$DEST_NAME"
+            link_recurse "$SRC/*" "$DEST/$DEST_NAME"
+        else
+            echo Linking $SRC to $DEST
+            ln -sf "$SRC" "$DEST/$DEST_NAME"
+        fi
+    done
+}
+
+# home dotfiles
+link_recurse $(find $BASEDIR -name ".*" -and -not -name ".git*" -and -not -name ".DS_Store" -depth 1) $HOME
+
+SUBLIME_DEST="${HOME}/Library/Application Support/Sublime Text 3/Packages"
+link_recurse "$BASEDIR/sublime/User" "$SUBLIME_DEST"

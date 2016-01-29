fc18058a03ecc1cf848dcb3963b4acd1273652d1
Ted Schundler <tschundler@gmail.com>
2016-01-28 21:37:06 -0800

    bash3 vs bash4 :/

--- makelinks.sh
+++ makelinks.sh
@@ -8,11 +8,12 @@ link_recurse() {
     local DEST SRC n DEST_NAME
     DEST=${*: -1:1}
     n=${#*}
+
     # shell array voodoo... shouldn't it be n-1?
-    for SRC in ${*::n}; do
+    for SRC in ${*:1:n-1}; do
     	DEST_NAME=$(basename "$SRC")
         if [ -d "$SRC" ]; then
-            #echo $SRC is DIR
+            echo $SRC is DIR
             mkdir -p "$DEST/$DEST_NAME"
             link_recurse "$SRC/*" "$DEST/$DEST_NAME"
         else
@@ -23,7 +24,7 @@ link_recurse() {
 }
 
 # home dotfiles
-link_recurse $(find $BASEDIR -name ".*" -and -not -name ".git*" -and -not -name ".DS_Store" -depth 1) $HOME
+link_recurse $(find $BASEDIR -maxdepth 1 -name ".*" -and -not -name ".git*" -and -not -name ".DS_Store" -and -not -name ".") $HOME
 
 if [ -d ${HOME}/.config/sublime-text-3/Packages/User ]; then
     # UN*X
